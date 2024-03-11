6cf8b027653404135d1f58642fb1da6cf3536d7f
Manoj Biswal <134379676+Manj-2343@users.noreply.github.com>
2024-03-11 09:34:40 +0530

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+export default {
+  css: ["/css/utils.css", "/css/media.css", "/css/custom.css"],
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config: any, ctx: any) {},
+  },
+};
